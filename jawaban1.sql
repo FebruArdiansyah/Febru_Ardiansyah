458316f5cca80a65a2f046e1e96454b40c238c3b
FebruArdiansyah <151010769+FebruArdiansyah@users.noreply.github.com>
2023-11-23 09:27:54 +0700

    jawaban1.sql

--- jawaban1.sql
+++ jawaban1.sql
@@ -10,7 +10,7 @@ CREATE TABLE jurusan (
 CREATE TABLE mahasiswa (
     id INT PRIMARY KEY,
     First_name VARCHAR(100) NOT NULL,
-		Last_name VARCHAR(100) NOT NULL,
+    Last_name VARCHAR(100) NOT NULL,
     nim_mahasiswa VARCHAR(100) NOT NULL,
     kode_matkul_id INT,
     FOREIGN KEY (kode_matkul_id) REFERENCES matakuliah(id)
@@ -21,14 +21,14 @@ CREATE TABLE dosen (
     id INT PRIMARY KEY,
     kode_dosen VARCHAR(11) NOT NULL,
     nama_dosen VARCHAR(100) NOT NULL,
-		kode_matkul_id INT,
-		FOREIGN KEY kode_matkul(id) REFERENCES matakuliah(id)
+    kode_matkul_id INT,
+    FOREIGN KEY kode_matkul(id) REFERENCES matakuliah(id)
 );
 
 CREATE TABLE matakuliah (
     id INT PRIMARY KEY,
     kode_matkul VARCHAR(11) NOT NULL,
     nama_matkul VARCHAR(100) NOT NULL,
-		matakuliah_id INT,
+    matakuliah_id INT,
     jurusan_id INT
 );
